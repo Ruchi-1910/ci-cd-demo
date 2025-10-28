f6621349b428a865e80b5a1494a5322f6c697c25
Ruchita Salunkhe <ruchitasalunkhe19@gmail.com>
2025-10-28 20:22:04 +0530

    Added app.py and deploy.sh for CI/CD simulation

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "-----CI/CD Pipeline Started-----"
+
+echo "Step 1: Building the Application..."
+sleep 2
+
+echo "Step 2: Running Tests..."
+sleep 2
+echo "Tests passed successfully!"
+
+echo "Step 3: Deploying Application..."
+sleep 2
+echo "Application deployed successfully!"
+
+echo "------CI/CD Pipeline Finished------"
